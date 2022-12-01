fceae10d00514631f3ff7da647f7bc9955055224
Mario De Luca <bloodcreed21@gmail.com>
2022-12-01 18:31:11 +0100

    first commit

---
+++ first_day/app.py
@@ -0,0 +1,14 @@
+import functools
+
+total_list = []
+with open("./lista_elfi.txt", "r") as file:
+    total_list = file.read().split("\n\n")
+
+total_sum = [functools.reduce(lambda x, y: x + y, [int(x) for x in el.split("\n")]) for el in total_list]
+
+total_sum.sort(reverse=True)
+
+sum_of_first_3 = total_sum[0] + total_sum[1] + total_sum[2]
+
+print(sum_of_first_3)
+
