bfbed274a55f11dc4ed2f78f19b95b349378df82
Mario De Luca <bloodcreed21@gmail.com>
2022-12-08 18:08:35 +0100

    to finish

--- eigth_day/first_part.py
+++ eigth_day/first_part.py
@@ -29,64 +29,69 @@ def get_all_edge_visibile_trees(matrix):
 
 # A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. 
 # Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
-def treeIsVisibileInADirection(x,y, matrix):
-    if look_direction(x,y, matrix, Direction.UP):
-        print("UP")
-        return True
-        
-    if look_direction(x,y, matrix, Direction.DOWN):
-        print("DOWN")
+def isTreeVisibile(x : int,y : int, matrix : list[list[int]]) -> bool:
+    """Check if the tree is visibile in a direction"""
+    if isOnTheEdge(x,y,matrix):
         return True
 
-    if look_direction(x,y, matrix, Direction.LEFT):
-        print("LEFT")
+    if check_direction_visibile_left_right(x,y,matrix):
+        print("LEFT OR RIGHT APERTURE")
         return True
 
-    if look_direction(x,y, matrix, Direction.RIGHT):
-        print("RIGHT")
+    if check_direction_visibile_up_down(x,y,matrix):
+        print("UP OR DOWN APERTURE")
         return True
-    return False
 
+    print("NO APERTURE")
+    return False    
 
-def look_direction(x,y, matrix, direction):
-    current_tree = matrix[x][y]
+def check_direction_visibile_up_down(x: int, y: int, matrix: list[list[int]], ) -> bool:
+    """Check if the tree is visibile in the up and down direction"""
+    all_trees = []
+    for i in range(0, len(matrix)):
+        all_trees.append(matrix[i][y])
+
+    any_tree_is_higher = any([tree > matrix[x][y] for tree in all_trees])
+    if any_tree_is_higher:
+        return False
     
-    match direction:
-        case Direction.UP:
-            for i in range(x-1, 0, -1):
-                target_tree = matrix[i][y]
-                if target_tree < current_tree:
-                    return True
-            return False
-        case Direction.DOWN:
-            for i in range(x+1, len(matrix)):
-                target_tree = matrix[i][y]
-                if target_tree < current_tree:
-                    return True
-            return False
-        case Direction.LEFT:
-            for i in range(y-1, 0, -1):
-                target_tree = matrix[x][i]
-                if target_tree < current_tree:
-                    return True
-            return False
-        case Direction.RIGHT:
-            for i in range(y+1, len(matrix[0])):
-                target_tree = matrix[x][i]
-                if target_tree < current_tree:
-                    return True
-            return False
-            
+    return True
+
+
+def check_direction_visibile_left_right(x: int, y: int, matrix: list[list[int]], ) -> bool:
+    """Check if the tree is visibile in the left and right direction"""
+    all_trees = []
+    for i in range(0, len(matrix[0])):
+        all_trees.append(matrix[x][i])
+    any_tree_is_higher = any([tree >  matrix[x][y] for tree in all_trees])
+    print(any_tree_is_higher)
+    print(all_trees)
+    print(matrix[x][y])
+
+    if any_tree_is_higher:
+        return False
+    
+    return True
+
 
 visibileTress = get_all_edge_visibile_trees(matrix)
 
-for x in range(len(matrix)):
-    for y in range(len(matrix[0])):
-        if isOnTheEdge(x,y,matrix):
-            continue
+# for x in range(len(matrix)):
+#     for y in range(len(matrix[0])):
+#         if isOnTheEdge(x,y,matrix):
+#             continue
 
-        if treeIsVisibileInADirection(x,y,matrix):
-            visibileTress.append((x,y))
+#         if isTreeVisibile(x,y,matrix):
+#             visibileTress.append((x,y))
             
-print(len(get_all_edge_visibile_trees(matrix)))
-print(len(visibileTress))
+# print(len(get_all_edge_visibile_trees(matrix)))
+# print(len(visibileTress))
+# print(visibileTress)
+
+# TODO dividere left, right, up, down altrimenti in casi particolari non funziona
+# TODO controllare se la tree è visibile in tutte le direzioni
+isTreeVisibile(2,1,matrix)
+print("----")
+isTreeVisibile(2,2,matrix)
+print("----")
+isTreeVisibile(2,3,matrix)
