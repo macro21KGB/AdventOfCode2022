65ab74a48d1e2e417c67f24b5dc91bac593087ba
Mario De Luca <bloodcreed21@gmail.com>
2022-12-09 01:22:52 +0100

    add: day 8 solved

--- eigth_day/second_part.py
+++ eigth_day/second_part.py
@@ -19,71 +19,64 @@ def get_all_edge_visibile_trees(matrix):
                 visibileTress.append((x,y))
     return visibileTress
 
+
+
 # A tree is visible if all of the other trees between it and an edge of the grid are shorter than it. 
 # Only consider trees in the same row or column; that is, only look up, down, left, or right from any given tree.
-def isTreeVisibile(x : int,y : int, matrix : list[list[int]]) -> bool:
+def generate_view_score(x : int,y : int, matrix : list[list[int]]) -> bool:
     """Check if the tree is visibile in a direction"""
-    
     if isOnTheEdge(x,y,matrix):
         return True
 
-    visibile = False
+    view_top = count_visibile_tree_of_view(x, y, matrix, range(x-1, -1, -1), horizontal=False)
+    view_bottom = count_visibile_tree_of_view(x, y, matrix, range(x+1, len(matrix)), horizontal=False)
+    view_left = count_visibile_tree_of_view(x, y, matrix, range(y-1, -1, -1), horizontal=True)
+    view_right = count_visibile_tree_of_view(x, y, matrix, range(y+1, len(matrix[0])), horizontal=True)
 
-    if check_if_tree_is_visibile_in_range(x, y, matrix, range(x-1, -1, -1), horizontal=False):
-        visibile = True
+    return view_top * view_bottom * view_left * view_right
 
-    if check_if_tree_is_visibile_in_range(x, y, matrix, range(x+1, len(matrix)), horizontal=False):
-        visibile = True
-    
-    if check_if_tree_is_visibile_in_range(x, y, matrix, range(y-1, -1, -1), horizontal=True):
-        visibile = True
 
-    if check_if_tree_is_visibile_in_range(x, y, matrix, range(y+1, len(matrix[0])), horizontal=True):
-        visibile = True
-
-    return visibile    
-
-def check_if_tree_is_visibile_in_range(x: int, y: int, matrix: list[list[int]], target_range: range, horizontal: bool = True):
+ 
+def count_visibile_tree_of_view(x: int, y: int, matrix: list[list[int]], target_range: range, horizontal: bool = True) -> int:
     """Check if the tree is visibile in a range"""
     all_trees : list[str] = []
+    tree_view_count = 0
     current_tree_value = int(matrix[x][y])
 
     if horizontal:
         for i in target_range:
             all_trees.append(int(matrix[x][i]))
 
-        if len(all_trees) == 0:
-            return False
 
         for target_tree in all_trees:
-            if int(target_tree) >= int(matrix[x][y]):
-                return False
+            if current_tree_value > int(target_tree):
+                tree_view_count += 1
+            if current_tree_value <= int(target_tree):
+                tree_view_count += 1
+                break
 
-        return True
+        return tree_view_count
     else:
         for i in target_range:
             all_trees.append(int(matrix[i][y]))
 
-        if len(all_trees) == 0:
-            return False
-
         for target_tree in all_trees:
-            if int(target_tree) >= current_tree_value:
-                return False
-        
-        return True
+            if current_tree_value > int(target_tree):
+                tree_view_count += 1
+            if current_tree_value <= int(target_tree):
+                tree_view_count += 1
+                break
+
+        return tree_view_count
 
 
 
-visibileTress = get_all_edge_visibile_trees(matrix)
-
+scores = []
 for x in range(len(matrix)):
     for y in range(len(matrix[0])):
         if isOnTheEdge(x,y,matrix):
             continue
-
-        if isTreeVisibile(x,y,matrix):
-            visibileTress.append((x,y))
             
-print(len(visibileTress))
+        scores.append(generate_view_score(x,y,matrix))
 
+print(max(scores))
